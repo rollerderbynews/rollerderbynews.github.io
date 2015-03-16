2b3a8d58303bd425628df28070f5a2e79bd48224
Riccardo Donato <riccardo.donato@gmail.com>
2015-03-16 23:16:59 +0100

    iii

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,16 @@
 $(function() {
 
+    var timerId =
+  countdown(
+    new Date(),
+    function(ts) {
+      document.getElementById('pageTimer').innerHTML = ts.toHTML("strong");
+    },
+    countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
+
+// later on this timer may be stopped
+window.clearInterval(timerId);
+
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
