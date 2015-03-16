0a7330023d0c5090c2792b5029f5433141621bc2
Riccardo Donato <riccardo.donato@gmail.com>
2015-03-17 00:28:42 +0100

    Search

--- js/search.js
+++ js/search.js
@@ -3,7 +3,7 @@
 var nextPageToken = ''
 // Search for a specified string.
 function search() {
-  var q = 'rollerderby';//$('#query').val();
+  var q = 'rollerderby italy';//$('#query').val();
   var request = gapi.client.youtube.search.list({
     q: q,
     part: 'snippet',
